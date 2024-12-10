79b9d688c2f96a0d49f4657aec384f873a77b18b
BradSavary <brad.savary@etu.unilim.fr>
2024-12-10 08:52:17 +0100

    modified:   api/Repository/EntityRepository.php 	modified:   client/src/lib/api-request.js 	modified:   api/Repository/EntityRepository.php modified:   api/index.php 	modified:   client/index.html 	modified:   client/src/lib/utils.js 	modified:   client/src/main.js 	deleted:    client/src/ui/header/index.js 	deleted:    client/src/ui/header/template.html

--- client/src/lib/api-request.js
+++ client/src/lib/api-request.js
@@ -4,7 +4,7 @@
  */
 
 
-let API_URL = "TO BE DEFINED"; // URL de base de l'API (à définir)
+let API_URL = "https://mmi.unilim.fr/~savary23/SAE303/api/"; // URL de base de l'API (à définir)
 
 
 /**
